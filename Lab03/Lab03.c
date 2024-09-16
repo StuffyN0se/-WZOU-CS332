2a4741d16d6d73fcea1f89e782602b6acc52fc19
StuffyN0se <150478662+StuffyN0se@users.noreply.github.com>
2024-09-15 19:13:10 -0500

    Update Lab03.c

--- Lab03/Lab03.c
+++ Lab03/Lab03.c
@@ -50,7 +50,6 @@ int main(int argc, char** argv){
     displayStrings(str, numStr);
     printf("\n");
 
-    // Free dynamically allocated memory
     for (int i = 0; i < numStr; i++) {
         free(str[i]);
     }
