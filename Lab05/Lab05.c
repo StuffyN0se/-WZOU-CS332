683ffa07f2fc098a002f4e0e594a00da87636b89
StuffyN0se <150478662+StuffyN0se@users.noreply.github.com>
2024-09-30 00:23:09 +0000

    Finished Lab05

--- Lab05/Lab05.c
+++ Lab05/Lab05.c
@@ -7,84 +7,64 @@
 #include <sys/stat.h>
 #include <dirent.h>
 
+//identifies the type of file 
 char *filetype(unsigned char type) {
-char *str;
-switch(type) {
-case DT_BLK: str = "block device"; break;
-case DT_CHR: str = "character device"; break;
-case DT_DIR: str = "directory"; break;
-case DT_FIFO: str = "named pipe (FIFO)"; break;
-case DT_LNK: str = "symbolic link"; break;
-case DT_REG: str = "regular file"; break;
-case DT_SOCK: str = "UNIX domain socket"; break;
-case DT_UNKNOWN: str = "unknown file type"; break;
-default: str = "UNKNOWN";
+    char *str;
+    switch(type) {
+        case DT_BLK: str = "block device"; break;
+        case DT_CHR: str = "character device"; break;
+        case DT_DIR: str = "directory"; break;
+        case DT_FIFO: str = "named pipe (FIFO)"; break;
+        case DT_LNK: str = "symbolic link"; break;
+        case DT_REG: str = "regular file"; break;
+        case DT_SOCK: str = "UNIX domain socket"; break;
+        case DT_UNKNOWN: str = "unknown file type"; break;
+        default: str = "UNKNOWN";
+    }
+    return str;
 }
-return str;
+
+//function to recursively go through directories and their subdirectories
+void traverse_directory(const char *directory) {
+    //creates strut to store directories
+    struct dirent *dirent;
+    //parent directory is user input 
+    DIR *parentDir = opendir(directory);
+
+    if (parentDir == NULL) {
+        printf ("Error opening directory '%s'\n", directory);
+        return;
+    }
+    while((dirent = readdir(parentDir)) != NULL){
+
+        //sees if directory is . or . or starts with . and skips them
+        if (strcmp(dirent->d_name, ".") == 0 || strcmp(dirent->d_name, "..") == 0 || dirent->d_name[0] == '.') {
+            continue; 
+        }
+        //prints out the current file name and file type
+        printf ("   -%s (%s)\n", dirent->d_name, filetype(dirent->d_type));
+
+        //creates buffer to store the current filePath
+        char filePath[1000];
+        snprintf(filePath, sizeof(filePath), "%s%s", directory, dirent->d_name);
+
+        //if current file is a directory call on function recursively to go through the directory
+        if (dirent->d_type == DT_DIR) {
+        printf("Opening Directory: %s\n", dirent->d_name);
+        traverse_directory(filePath); 
+        }
+    }
+closedir (parentDir);
 }
 int main (int argc, char **argv) {
-struct dirent *dirent;
-DIR *parentDir;
-if (argc < 2) {
-printf ("Usage: %s <dirname>\n", argv[0]);
-exit(-1);
+     if (argc < 2) {
+        printf("Usage: %s <directory>\n", argv[0]);
+        exit(-1);
+    }
+    // Start traversal from the directory user inputted
+    traverse_directory(argv[1]);
+    return 0;
 }
-parentDir = opendir (argv[1]);
-if (parentDir == NULL) {
-printf ("Error opening directory '%s'\n", argv[1]);
-exit (-1);
-}
-int count = 1;
-while((dirent = readdir(parentDir)) != NULL){
-printf ("[%d] %s (%s)\n", count, dirent->d_name, filetype(dirent->d_type));
-count++;
-}
-
-int fd;
-struct stat statbuf;
-if ((fd = open(argv[1], O_RDONLY)) == -1) {
-printf("Error opening file %s\n", argv[1]);
-perror("open");
-exit(-1);
-}
-read(fd, &statbuf, sizeof(struct stat));
-close(fd);
-printstat(statbuf);
-
-void printstat(struct stat statbuf);
-
-int i;
-struct stat buf;
-char *ptr;
-for (i = 1; i < argc; i++) {
-printf("%s: ", argv[i]);
-if (lstat(argv[i], &buf) < 0) {
-printf("lstat error");
-continue;
-}
-if (S_ISREG(buf.st_mode))
-ptr = "regular";
-else if (S_ISDIR(buf.st_mode))
-ptr = "directory";
-else if (S_ISCHR(buf.st_mode))
-ptr = "character special";
-else if (S_ISBLK(buf.st_mode))
-ptr = "block special";
-else if (S_ISFIFO(buf.st_mode))
-ptr = "fifo";
-else if (S_ISLNK(buf.st_mode))
-ptr = "symbolic link";
-else if (S_ISSOCK(buf.st_mode))
-ptr = "socket";
-else
-ptr = "** unknown mode **";
-printf("%s\n", ptr);
-printstat(buf);
-}
-closedir (parentDir);
-return 0;
-}
-
 
 
 
