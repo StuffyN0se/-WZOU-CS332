84f1a65a93c13444fadb0582087a1107921e26bb
StuffyN0se <150478662+StuffyN0se@users.noreply.github.com>
2024-11-03 16:33:50 -0600

    redirected stdout and stderr

--- Lab10/Lab10.c
+++ Lab10/Lab10.c
@@ -25,10 +25,22 @@ int main(int argc, char **argv) {
     char line[BUFSIZ], buf[BUFSIZ], *args[BUFSIZ];
     time_t t1, t2;
 
-    if (argc < 2) {
+    if (argc < 2){
         printf("Usage: %s <commands file>\n", argv[0]);
         exit(-1);
     }
+    /* open file to read standard input stream,
+    make sure the file stdin.txt exists, even if it is empty */
+    if ((output = open("output.txt", O_CREAT | O_APPEND | O_WRONLY, 0755)) == -1) {
+	printf("Error opening file output.txt for output\n");
+	exit(-1);
+	}
+     /* open file to write standard output stream in append mode.
+     create a new file if the file does not exist. */
+     if ((input = open("error.txt", O_CREAT | O_APPEND | O_WRONLY, 0755)) == -1) {
+	printf("Error opening file error.txt for error\n");
+	exit(-1);
+     }
 
     FILE *fp1 = fopen(argv[1],"r");
     if (fp1 == NULL) {
@@ -55,6 +67,24 @@ int main(int argc, char **argv) {
       time(&t1);
       pid = fork();
       if (pid == 0) { /* this is child process */
+	      
+	/* replace standard input stream with the file stdin.txt */
+	dup2(output, 1);
+	/* replace standard output stream with the file stdout.txt */
+	dup2(error, 2);
+	execvp(argv[1], &argv[1]);
+	/* since stdout is written to stdout.txt and not the terminal,
+	we should write to stderr in case exec fails, we use perror
+	that writes the error message to stderr */
+
+	perror("exec");
+	exit(-1);
+	} else if (pid > 0) { /* this is the parent process */
+	/* output from the parent process still goes to stdout :-) */
+	printf("Wait for the child process to terminate\n");
+	wait(&status); /* wait for the child process to terminate */
+	if (WIFEXITED(status)) { /* child process terminated normally */
+
         execvp(args[0], args);
 	perror("exec");
 	exit(-1);
